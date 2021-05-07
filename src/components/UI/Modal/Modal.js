a8e1924ab3bc5f5f43249359a5aa7e5121fa154e
itshmnt <hemantxs5@gmail.com>
2021-05-07 21:06:55 +0530

    Added SideDrawer Toggle

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,10 +4,10 @@ import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
+const modal = ( props ) => (
     <Aux>
         <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div 
+        <div
             className={classes.Modal}
             style={{
                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
