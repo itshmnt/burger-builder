f934be81597974eecafca6f05a3ad455a8719adb
itshmnt <hemantxs5@gmail.com>
2021-05-14 16:09:04 +0530

    Sending a POST Request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-conast instance = axios.create({
+const instance = axios.create({
     baseURL: 'https://burger-builder-hemantxs5-default-rtdb.asia-southeast1.firebasedatabase.app/'
-});
+})
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../axios-orders';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -82,7 +82,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!');
+        // alert('You Continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Hemant',
+                address: {
+                    street: 'TestStreet 1',
+                    zipCode: '415416',
+                    country: 'India'
+                },
+                email: 'hemantxs5@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
