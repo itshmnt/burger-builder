2c4dd0358d34b669e4ba8ce6f200415d4c3e1fb0
itshmnt <hemantxs5@gmail.com>
2021-06-03 20:46:13 +0530

    Handling Overall Form Validity and Fixing a Bug

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,9 +6,10 @@ const input = ( props ) => {
     let inputElement = null;
     const inputClasses = [classes.InputElement];
 
-    if (props.invalid && props.shouldValidate) {
+    if (props.invalid && props.shouldValidate && props.touched) {
         inputClasses.push(classes.Invalid);
     }
+
     switch ( props.elementType ) {
         case ( 'input' ):
             inputElement = <input
